d7ddef05da6a86be16ae3791a398a93ef267d183
Arpan Tomar <arpantomar2018@gmail.com>
2024-08-11 18:28:20 +0530

    frontend url setting changed

--- backend/index.js
+++ backend/index.js
@@ -2,20 +2,22 @@ import express from "express";
 import router from "./routes/router.js";
 import cors from "cors";
 import connectDB from "./db.js";
-const app=express();
+import dotenv from "dotenv";
+dotenv.config()
+const app = express();
 app.use(express.json());
 connectDB();
+const frontendUrl = process.env.FRONTEND_URL;
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
+    origin:frontendUrl,
+    methods: ["GET", "POST", "PUT", "DELETE"
   })
 );
-app.use("/",router);
-app.get("/",(req,res)=>{
-    res.send("hello");
-})
-app.listen(5000,()=>{
-    console.log("app running at 5000");
+app.use("/", router);
+app.get("/", (req, res) => {
+  res.send("hello");
+});
+app.listen(5000, () => {
+  console.log("app running at 5000");
 });
