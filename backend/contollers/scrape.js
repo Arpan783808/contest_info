5c1b760b5f878f648144950ace62b9e7d28bb0b9
Arpan Tomar <arpantomar2018@gmail.com>
2024-08-12 13:29:55 +0530

    pupperteer changed

--- backend/contollers/scrape.js
+++ backend/contollers/scrape.js
@@ -67,7 +67,18 @@ export async function scrapeAtcoderProfile(username) {
 }
 
 export async function scrapeAtcoderContests() {
-  const browser = await puppeteer.launch({ headless: true });
+  const browser = await puppeteer.launch({
+    args: [
+      "--disable-setuid-sandbox",
+      "--no-sandbox",
+      "--single-process",
+      "--no-zygote,",
+    ],
+    executablePath:
+      process.env.NODE_ENV === "production"
+        ? process.env.PUPPETEER_EXECUTABLE_PATH
+        : puppeteer.executablePath(),
+  });
   const page = await browser.newPage();
   const contests = { upcoming: [], past: [] };
 
